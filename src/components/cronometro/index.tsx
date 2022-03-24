b34292f8438084f0d48f7f0768ee7525b6e7531e
Letícia Leal <leticialeal@ciandt.com>
2022-03-24 15:18:12 -0300

    iniciando estudos de react -  construção da lista de estudos

---
+++ src/components/cronometro/index.tsx
@@ -0,0 +1,33 @@
+import Botao from "../button";
+import Relogio from "./relogio";
+import style from './Cronometro.module.scss';
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
+
+interface Props{
+    selecionado: ITarefa | undefined
+}
+
+export default function Cronometro({ selecionado } : Props){
+    const [tempo, setTempo] = useState<number>();
+
+    useEffect(() => {
+        if(selecionado?.tempo){
+            setTempo(tempoParaSegundos(selecionado.tempo));
+        }
+    }, [selecionado])
+
+    return(
+        <div className={style.cronometro}>
+            <p className={style.titulo}>Escolha um card e inicie o crônometro</p>
+            <div className={style.relogioWrapper}>
+                <Relogio tempo={tempo} />
+            </div>
+            <Botao>
+                Começar
+            </Botao>
+        </div>
+
+    )
+}
---
+++ src/pages/App.tsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import Cronometro from '../components/cronometro';
+import Formulario from '../components/formulario';
+import Lista from '../components/lista';
+import { ITarefa } from '../types/tarefa';
+import style from './App.module.scss';
+
+
+function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[] | []>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa){
+    setSelecionado(tarefaSelecionada);
+    setTarefas(tarefasAteriores => tarefasAteriores.map(tarefa => ({
+      ...tarefa,
+      selecionado: tarefa.id === tarefaSelecionada.id ? true : false
+    })));
+  }
+
+  return (
+    <div className={style.AppStyle}>
+    <Formulario setTarefas={setTarefas}/>
+    <Lista 
+    tarefas={tarefas}
+    selecionaTarefa={selecionaTarefa}
+    />
+    <Cronometro selecionado={selecionado}/>
+    </div>
+  );
+}
+
+export default App;
